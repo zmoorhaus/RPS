c206ee2c6df860b387a4a15987aa74cfe0382005
Zachariah Moorhaus <zmoorhaus@gmail.com>
2024-08-09 09:29:33 -0400

    Create a function that will play a single round of rock, paper scissors and will give a point to the winner.

--- rps.js
+++ rps.js
@@ -1,28 +1,62 @@
 console.log("ayyy")
 
+let computerScore = 0;
+let humanScore = 0;
+
 function getComputerChoice() {
 
-   let computerChoice = '';
-   let number = Math.floor(Math.random() * 3);
-   if (number === 0) {
-    computerChoice = "rock";
-   } else if (number === 1) {
-    computerChoice = "paper";
-   } else if (number === 2) {
-    computerChoice = "scissors";
-   }
-   return computerChoice
     
-}
+    let number = Math.floor(Math.random() * 3);
+    let computerChoice;
 
-function getHumanChoice() {
-    let humanChoice = prompt("Rock, Paper, or Scissors?")
-    if (humanChoice.toLowerCase() === "rock") {
-    console.log("You chose rock"); 
-    } else if (humanChoice.toLowerCase() === "paper") {
-    console.log("You chose paper");
-    } else if (humanChoice.toLowerCase() === "scissors") {
-    console.log("You chose scissors")
+    if (number === 0) {
+    computerChoice = "rock";
+    } else if (number === 1) {
+    computerChoice = "paper";
+    } else if (number === 2) {
+     computerChoice = "scissors";
+    }
+    console.log("Computer chose " + computerChoice);
+    return computerChoice;
+     
+ }
+ 
+ function getHumanChoice() {
+     let humanChoice = prompt("Rock, Paper, or Scissors?")
+     if (humanChoice.toLowerCase() === "rock") {
+     console.log("You chose rock"); 
+     } else if (humanChoice.toLowerCase() === "paper") {
+     console.log("You chose paper");
+     } else if (humanChoice.toLowerCase() === "scissors") {
+     console.log("You chose scissors")
+     
+     
+     }
+     return humanChoice;
+ }
+
+
+function playRound() {
+
+    let userChoice = getHumanChoice();
+    let computerChoice = getComputerChoice();
+    
+
+    if ((computerChoice === "rock" && userChoice === "scissors") 
+    || (computerChoice === "paper" && userChoice === "rock" )
+    || (computerChoice === "scissors" && userChoice === "paper")) {
+    computerScore++
+    alert(`Computer wins!\nComputer: ${computerScore}\nYou: ${humanScore}`)
+    } else if ((computerChoice === "rock" && userChoice === "paper") 
+    || (computerChoice === "paper" && userChoice === "scissors")
+    || (computerChoice === "scissors" && userChoice === "rock")) {
+    humanScore++
+    alert(`You win! Computer: ${computerScore}, you: ${humanScore}`)
+    } else if (computerChoice === userChoice) {
+        alert("TIE")
+    } else {
+        return "ERROR";
     }
 }
-getHumanChoice();
+
+playRound();
